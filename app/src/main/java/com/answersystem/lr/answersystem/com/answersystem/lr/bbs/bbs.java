72069eda7236ace49dbdab4a940f661d556d2a15
rowsmoe <1026960839@qq.com>
2018-04-18 00:07:31 +0800

    recyclerview

--- app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/bbs/bbs.java
+++ app/src/main/java/com/answersystem/lr/answersystem/com/answersystem/lr/bbs/bbs.java
@@ -2,6 +2,8 @@ package com.answersystem.lr.answersystem.com.answersystem.lr.bbs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
 
 import com.answersystem.lr.answersystem.R;
 
@@ -11,5 +13,6 @@ public class bbs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bbs);
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
     }
 }
